3f92297b3840b0e3a849a41e95fca85d58d6dc01
kingofspace0wzz <zizhuang_wang@umail.ucsb.edu>
2017-10-25 01:41:29 -0700

    SemiNMF & ConvexNMF;SVD-based initialization & latent rank selection

--- nmf.py
+++ nmf.py
@@ -21,10 +21,15 @@ def semi_nmf(x, iter=1000):
     '''
     x = x.numpy()
     f, gT, p = svd_initialization(x)
+    
     for i in range(iter):
+
         f = np.dot(x, np.dot(gT.T, la.inv(np.dot(gT, gT.T))))
+
         for j in range(gT.shape[0]):
+
             for k in range(gT.shape[1]):
+
                 gT[j,k] = gT[j,k] * np.sqrt( ( ( abs(np.dot(x.T, f))[j,k] + np.dot(x.T, f)[j,k] )/2  +  np.dot(gT.T, (abs(np.dot(f.T, f)) - np.dot(f.T, f))/2)[j,k] )
                     /  ( ( abs(np.dot(x.T, f))[j,k] - np.dot(x.T, f)[j,k] )/2  +  np.dot(gT.T, (abs(np.dot(f.T, f)) + np.dot(f.T, f))/2)[j,k] )  ) # BUG to be fixed
 
@@ -45,19 +50,22 @@ def svd_initialization(x):
     p, sum_p = 0, 0
     U, s, Vh = la.svd(x)
     sum_r = sum(s)
+
     for i in range(len(s)):
+
         if sum_p/sum_r < 0.9:
             sum_p = sum_p + s[i]
             p+=1
 
     sigma = np.zeros((p, x.shape[1]))
+
     for i in range(p):
         sigma[i,i] = s[i]
 
     return abs(U[:, 0:p]), np.dot(sigma, Vh), p
 
 
-------------------test------------------
+#------------------test------------------
 
 def main():
 
